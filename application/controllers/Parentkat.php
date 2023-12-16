f4763608fe70b1a2a75b78b750c5b2eaca03fd86
ojik <ozjibuwat@gmail.com>
2023-12-16 08:30:37 +0700

    baru~

--- application/controllers/Parentkat.php
+++ application/controllers/Parentkat.php
@@ -21,18 +21,26 @@ class ParentKat extends CI_Controller {
    public function tambah(){
     $this->load->model('M_parent');
     $id_user = $this->input->post('id_user');
-    $parket = $this->input->post('kategoriParent');
-    $persen = $this->input->post('persentase');
-    $data = [
-        'id_user' => $id_user,
-        'kategori_parent' => $parket,
-        'persentase' => $persen
-    ];
-    $this->M_parent->insert($data,'kategori_parent');
-    redirect('parentkat/index');
-   }
+    $kategori = $this->input->post('kategoriParent');
+    $persentase = $this->input->post('persentase');
 
-   public function logout(){
-       $this->simple_login->logout();
-   }        
+    // Menghitung total persentase yang sudah ada
+    $totalPersentase = $this->M_parent->getTotalPersentase($id_user);
+
+    // Memeriksa apakah total persentase setelah tambahan akan melebihi 100%
+    if ($totalPersentase + $persentase > 100) {
+        // Jika melebihi, tampilkan pesan kesalahan atau lakukan tindakan yang sesuai
+        $error_message = "Total persentase kategori_parent melebihi 100%";
+        // Tampilkan pesan kesalahan ke view atau lakukan redirect
+        redirect('parentkat/index/error/' . $error_message);
+    } else {
+        $data = [
+            'id_user' => $id_user,
+            'kategori_parent' => $kategori,
+            'persentase' => $persentase
+        ];
+        $this->M_parent->insert($data);
+        redirect('parentkat/index');
+    }
+}    
 }
--- application/models/M_parent.php
+++ application/models/M_parent.php
@@ -21,50 +21,12 @@
             }
             return $this->db->get()->result_array();
         }
+        public function getTotalPersentase($id_user) {
+            $this->db->select_sum('persentase');
+            $this->db->where('id_user', $id_user);
+            $query = $this->db->get($this->table);
+            return $query->row()->persentase;
+        }
+        
     }
-    //     public function get_data_pengeluaran($id = 0){
-    //         $this->db->select("*");
-    //         $this->db->from("kategori_transaksi");
-            
-    //         if($id != 0){
-    //             $this->db->where("id_user", $id);
-    //             $this->db->where("Deskripsi", "pengeluaran");
-    //         }
-        
-    //         return $this->db->get()->result_array();
-    //     }
-    //     public function get_data_pemasukkan($id = 0){
-    //         $this->db->select("*");
-    //         $this->db->from("kategori_transaksi");
-            
-    //         if($id != 0){
-    //             $this->db->where("id_user", $id);
-    //             $this->db->where("Deskripsi", "pemasukkan");
-    //         }
-        
-    //         return $this->db->get()->result_array();
-    //     }
-    //     public function delete($id) {
-    //         try {
-    //             $this->db->where("id_kategori", $id);
-    //             $this->db->delete("kategori_transaksi");
     
-    //             if ($this->db->affected_rows() > 0) {
-    //                 return true;
-    //             } else {
-    //                 return false;
-    //             }
-    //         } catch (Exception $e) {
-    //             // Log or handle the exception
-    //             log_message('error', 'Error deleting category: ' . $e->getMessage());
-    
-    //             // Return false and handle the error in the controller
-    //             return false;
-    //         }
-    //     }
-        
-
-       
-        
-        
-    // }
